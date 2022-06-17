e8923e511ce1b10aef3adf06c240cfebaa47da51
Kenji Suzuki <60703087+kvaluation@users.noreply.github.com>
2022-06-17 23:27:33 +0900

    5回配信対応でGoogleドライブへの共有を停止
    ダウンロードしたZipfiles等のGoogleドライブへの保存をコメントアウト

--- zipDownload.sh
+++ zipDownload.sh
@@ -12,12 +12,12 @@
 /Users/home/Library/Python/3.8/bin/probeCOCOATEK list -nk  > \
 /Users/home/Dropbox/EN/List/list`date "+%Y%m%d"`.txt 
 
-/Users/home/Library/Python/3.8/bin/probeCOCOATEK list -nk  > \
-/Volumes/GoogleDrive/共有ドライブ/home/ENonGoogleDrive/List/list`date "+%Y%m%d"`.txt 
+# /Users/home/Library/Python/3.8/bin/probeCOCOATEK list -nk  > \
+# /Volumes/GoogleDrive/共有ドライブ/home/ENonGoogleDrive/List/list`date "+%Y%m%d"`.txt 
 
 #TEK zip files
 /Users/home/Library/Python/3.8/bin/probeCOCOATEK dl \
 /Users/home/Dropbox/EN/Zipfiles
 
-/Users/home/Library/Python/3.8/bin/probeCOCOATEK dl \
-/Volumes/GoogleDrive/共有ドライブ/home/ENonGoogleDrive/Zipfiles
+# /Users/home/Library/Python/3.8/bin/probeCOCOATEK dl \
+# /Volumes/GoogleDrive/共有ドライブ/home/ENonGoogleDrive/Zipfiles
